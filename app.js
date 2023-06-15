1d620734e6d1123268462ebb037461fba6bbca25
junaid_07 <junaid.apsc@gmail.com>
2023-06-15 15:36:56 +0500

    Serve static files using express

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
+const morgan = require('morgan');
 
 const app = express();
+
 app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
+
 // Custom Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
