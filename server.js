f4487c996b90528bc4ef1b1131e0966191b669fe
junaid_07 <junaid.apsc@gmail.com>
2023-07-04 17:20:19 +0500

    Handle unhandled rejections outside express

--- server.js
+++ server.js
@@ -30,6 +30,15 @@ mongoose
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 🔒 Shutting down...');
+  // server.close first complete all the pending request and then the process.exit in the callback instantly closes the server. 1 in process.exit() means uncaught exception and 0 means success
+  server.close(() => {
+    process.exit(1);
+  });
 });
