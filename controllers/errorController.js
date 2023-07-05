47d0b3c7c5787c3d8be7db5421031d6a081a83bc
mjunaid07 <junaid.apsc@gmail.com>
2023-07-05 16:33:30 +0500

    Fix production|development errorrs blocks

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -51,8 +51,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
-    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    let error = Object.create(Object.getPrototypeOf(err));
+    Object.defineProperties(error, Object.getOwnPropertyDescriptors(err));
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
