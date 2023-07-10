f8979eb98df65aa7e0a87af0de0aefef42b93dde
junaid_07 <junaid.apsc@gmail.com>
2023-07-10 14:09:04 +0500

    Add new route and controller for user to update data other than the password

--- app.js
+++ app.js
@@ -26,15 +26,6 @@ app.use('/api/v1/users', userRouter);
 
 // Handling undefined routes
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`
-  // });
-
-  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
-
   // Whenver we pass anything into next express will assume there is an error and it will skip all the other middleware in the middlewares stack and will sent the error to the global error handling middleware
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(key => {
+    if (allowedFields.includes(key)) newObj[key] = obj[key];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -14,7 +23,34 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     }
   });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // Filtered out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+
 exports.createUser = (req, res) => {
+  console.log('body object', req.body);
   res.status(500).json({
     status: 'error',
     message: 'This route is not defined yet!'
