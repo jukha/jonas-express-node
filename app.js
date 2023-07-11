32b7174cd0e87fb2a99e08591e08fd04fb37798c
junaid_07 <junaid.apsc@gmail.com>
2023-07-11 11:37:26 +0500

    Set security HTTP headers using helemet package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,19 +10,27 @@ const tourRouter = require('./routes/tourRoutes');
 
 const app = express();
 
-// GLOBAL MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
 
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again after an hour!'
 });
 app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
 app.use(express.json());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Custom Middleware
