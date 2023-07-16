28252f5072970c21b5d1149b01c48b66709eb8ba
junaid_07 <junaid.apsc@gmail.com>
2023-07-16 17:37:34 +0500

    Add create and get Reviews functionality

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -15,20 +15,16 @@ const reviewSchema = new mongoose.Schema(
       type: Date,
       default: Date.now()
     },
-    tours: [
-      {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Tour',
-        required: [true, 'Review must belong to a tour.']
-      }
-    ],
-    users: [
-      {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: [true, 'Review must belong to a user.']
-      }
-    ]
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.']
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -38,4 +34,4 @@ const reviewSchema = new mongoose.Schema(
 
 const Review = mongoose.model('Review', reviewSchema);
 
-exports.module = Review;
+module.exports = Review;
