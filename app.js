0c7fe1bfff784d957369a6e1aa1d15bb07f9785a
junaid_07 <junaid.apsc@gmail.com>
2023-07-20 14:06:38 +0500

    Setup pug and Add header,footer templates

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,14 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GLOBAL MIDDLEWARES
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set security HTTP headers
 app.use(helmet());
 
@@ -54,9 +62,6 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Custom Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -64,6 +69,12 @@ app.use((req, res, next) => {
 });
 
 // Mounting the router
+app.get('/', (req, res) =>
+  res.status(200).render('base', {
+    tour: 'Placeholder tour'
+  })
+);
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
