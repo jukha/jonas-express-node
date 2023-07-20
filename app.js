f01cbe23aff63db338a2cea13cc7c3074db98a0c
junaid_07 <junaid.apsc@gmail.com>
2023-07-20 14:50:43 +0500

    Add route and controller file for views

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -69,12 +70,7 @@ app.use((req, res, next) => {
 });
 
 // Mounting the router
-app.get('/', (req, res) =>
-  res.status(200).render('base', {
-    tour: 'Placeholder tour'
-  })
-);
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
