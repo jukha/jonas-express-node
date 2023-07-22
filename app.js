5df5f0c4a6b972953fec0f6420ebd25cf04322a4
junaid_07 <junaid.apsc@gmail.com>
2023-07-22 19:18:53 +0500

    Implement updating user data by sending new data via form

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 // Data sanitization against NOSQL query injection
