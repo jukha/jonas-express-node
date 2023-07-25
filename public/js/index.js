d2c06a07ed29b82a784606facb24fd796141b197
junaid_07 <junaid.apsc@gmail.com>
2023-07-25 12:43:36 +0500

    Add bookig routes,controllers and Integrate stripe

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -10,22 +11,31 @@ const loginForm = document.querySelector('.login-form');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookTourBtn = document.getElementById('book-tour');
 
 // VALUES
-
-// DELEGATION
-if (mapBox) {
-  const locations = JSON.parse(dataset.locations);
-  displayMap(locations);
-}
-
 if (loginForm) {
+  console.log('yes login form is there');
   document.querySelector('.form').addEventListener('submit', e => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
   });
+}
+
+// DELEGATION
+if (bookTourBtn) {
+  bookTourBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
+  });
+}
+
+if (mapBox) {
+  const locations = JSON.parse(dataset.locations);
+  displayMap(locations);
 }
 
 if (logOutBtn) {
---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alert';
+const stripe = Stripe(
+  'pk_test_51NXKE5EL3qs5hVekpBwTWPmcuOchKLnfjznYnSsPtowZGb4TMpwhoOnGvLZxVcX6StX0Qjm7dgUhzI02Z29PCaCd000eAu3bRM'
+);
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (error) {
+    console.log(error);
+    showAlert('error', error);
+  }
+};
